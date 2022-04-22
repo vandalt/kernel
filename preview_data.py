ebe34b972ccbf4d8948e1155a4c8371767b33374
Thomas Vandal <thomasvandal@hotmail.com>
2022-04-22 16:22:49 -0400

    Add extra row to preview script

--- preview_data.py
+++ preview_data.py
@@ -2,50 +2,39 @@ from pathlib import Path
 
 from astropy.io import fits
 import matplotlib.pyplot as plt
-from matplotlib.colors import PowerNorm
+from matplotlib.colors import PowerNorm, LogNorm
 
-wbad_dir  = Path("pipeline_calibrated_data_wbadpix/")
-old_corr_dir = Path("pipeline_calibrated_data_wbadpix_corr/")
-custom_corr_dir = Path("pipeline_calibrated_data_wbadpix_corr_custom/")
-wf_corr_dir = Path("pipeline_calibrated_data_wbadpix_corr_wf/")
-amical_corr_dir = Path("pipeline_calibrated_data_wbadpix_corr_amical/")
+parent_data_dir = Path("data")
+wbad_dir  = parent_data_dir / Path("pipeline_calibrated_data_wbadpix/")
+old_corr_dir = parent_data_dir / Path("pipeline_calibrated_data_wbadpix_corr/")
+custom_corr_dir = parent_data_dir / Path("pipeline_calibrated_data_wbadpix_corr_custom/")
+wf_corr_dir = parent_data_dir / Path("pipeline_calibrated_data_wbadpix_corr_wf/")
+amical_corr_dir = parent_data_dir / Path("pipeline_calibrated_data_wbadpix_corr_amical/")
 
 corrections = {
     "No correction": wbad_dir,
-    "NRM correction (bad)": old_corr_dir,
+    # "NRM correction (bad)": old_corr_dir,
     "Custom mask": custom_corr_dir,
-    "WF mask":wf_corr_dir,
+    # "WF mask":wf_corr_dir,
     "AMICAL correction (Gaussian convolution)": amical_corr_dir,
 }
 
-files = [
-    "jw01093008001_01101_00006_nis_calints.fits",
-    "jw01093008001_01101_00007_nis_calints.fits",
-    "jw01093009001_01101_00006_nis_calints.fits",
-    "jw01093009001_01101_00007_nis_calints.fits",
-    "jw01093010001_01101_00006_nis_calints.fits",
-    "jw01093010001_01101_00007_nis_calints.fits",
-    "jw01093011001_01101_00006_nis_calints.fits",
-    "jw01093011001_01101_00007_nis_calints.fits",
-    "jw01093008001_01101_00006_nis_calints.fits",
-    "jw01093008001_01101_00007_nis_calints.fits",
-    "jw01093009001_01101_00006_nis_calints.fits",
-    "jw01093009001_01101_00007_nis_calints.fits",
-    "jw01093010001_01101_00006_nis_calints.fits",
-    "jw01093010001_01101_00007_nis_calints.fits",
-    "jw01093011001_01101_00006_nis_calints.fits",
-    "jw01093011001_01101_00007_nis_calints.fits",
-]
-
+files = [f.name for f in custom_corr_dir.glob("*calints.fits")]
 
 # %%
-ref_corr = corrections["Custom mask"]
+# ref_key = "Custom mask"
+ref_key = "No correction"
+ref_corr = corrections[ref_key]
+ref2_key = "Custom mask"
+ref2_corr = corrections[ref2_key]
 for fname in files:
 
-    fig, axs = plt.subplots(figsize=(19, 8), nrows=2, ncols=len(corrections), sharex="col")
+    fig, axs = plt.subplots(figsize=(19 * len(corrections) / 5, 1.5 * 8), nrows=3, ncols=len(corrections), sharex="col")
 
     fref = ref_corr / fname
     ref = fits.getdata(fref)
+    fref2 = ref2_corr / fname
+    ref2 = fits.getdata(fref2)
     for i, corr in enumerate(corrections):
 
         fpath = corrections[corr] / fname
@@ -53,14 +42,24 @@ for fname in files:
         data = fits.getdata(fpath)
 
         psf = axs[0, i].imshow(data[0], norm=PowerNorm(0.5, vmin=0), cmap="afmhot")
+        # psf = axs[0, i].imshow(data[0], norm=LogNorm(), cmap="afmhot")
         axs[0, i].set_title(corr)
 
-        res = axs[1, i].imshow(data[0] - ref[0], norm=PowerNorm(0.5, vmin=0), cmap="afmhot")
+        res = axs[1, i].imshow(data[0] - ref[0], norm=PowerNorm(0.1, vmin=0.0), cmap="afmhot")
+        # res = axs[1, i].imshow(data[0] - ref[0], cmap="afmhot")
+        # res = axs[1, i].imshow(data[0] - ref[0], norm=LogNorm(), cmap="afmhot")
         if i == 0:
-            axs[1, i].set_ylabel("Residuals vs. 'Custom mask'")
+            axs[1, i].set_ylabel(f"{corr} - {ref_key}")
+
+        res2 = axs[2, i].imshow(data[0] - ref2[0], norm=PowerNorm(0.1, vmin=0.0), cmap="afmhot")
+        # res = axs[1, i].imshow(data[0] - ref2[0], cmap="afmhot")
+        # res = axs[1, i].imshow(data[0] - ref2[0], norm=LogNorm(), cmap="afmhot")
+        if i == 0:
+            axs[2, i].set_ylabel(f"{corr} - {ref2_key}")
 
         fig.colorbar(psf, ax=axs[0, i], shrink=0.6)
         fig.colorbar(res, ax=axs[1, i], shrink=0.6)
+        fig.colorbar(res2, ax=axs[2, i], shrink=0.6)
     plt.tight_layout()
     plt.show()
     break
