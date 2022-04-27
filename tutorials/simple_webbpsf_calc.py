adef8a517131bdb8bd784eb9c0110c5203e4df9a
Thomas Vandal <thomasvandal@hotmail.com>
2022-04-26 20:46:10 -0400

    Add nircam to webbpsf example

--- tutorials/simple_webbpsf_calc.py
+++ tutorials/simple_webbpsf_calc.py
@@ -1,9 +1,19 @@
+# %%
 # Copied and adapted from first code cell here: https://webbpsf.readthedocs.io/en/latest/usage.html
 import webbpsf
 import matplotlib.pyplot as plt
 
+# %%
+# NIRISS
 ns = webbpsf.NIRISS()
 ns.filter = "F480M"
 ns.pupil_mask = "MASK_NRM"
 psf = ns.calc_psf(oversample=4, display=True)
+plt.show()
+
+# %%
+# NIRCam
+nc = webbpsf.NIRCam()
+nc.filter = "F480M"
+psf = nc.calc_psf(oversample=4, display=True)
 plt.show()
